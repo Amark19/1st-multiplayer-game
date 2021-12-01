3b0c5e717653e1c4073776ccb4033f0b011c2bd1
Amar K <amarkhamkar6@gmail.com>
2021-12-01 20:23:31 +0530

    multiplayer game added

---
+++ Assets/scripts/NetworkManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManager : Photon.PunBehaviour
+{
+
+    public GameObject Lobbycam;
+    public Transform spawnpoint;
+    public GameObject LobbyUI;
+    public Text statusText;
+
+    public const string Version = "1.0";
+    public const string RoomName = "Multiplayer";
+    public const string playerprefabName = "Player-1";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(Version);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        statusText.text = PhotonNetwork.connectionStateDetailed.ToString();
+    }
+
+    public override void OnConnectionFail(DisconnectCause cause){
+        print("connection Failed:" + cause.ToString());
+    }
+    public override void OnJoinedLobby(){
+        RoomOptions roomOptions = new RoomOptions() {isVisible = false,MaxPlayers = 4};
+        PhotonNetwork.JoinOrCreateRoom(RoomName,roomOptions,TypedLobby.Default);
+    }
+    public override void OnJoinedRoom(){
+        Lobbycam.SetActive(false);
+        LobbyUI.SetActive(false);
+
+        GameObject player = PhotonNetwork.Instantiate(playerprefabName,spawnpoint.position,spawnpoint.rotation,0);
+    }
+    public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer){
+        print("new Player Connected");
+    }
+    public override void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer){
+        print("Player DisConnected");
+    }
+
+}
---
+++ Assets/scripts/NetworkPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayer : Photon.MonoBehaviour
+{
+
+    public GameObject localCam;
+    PhotonView view;
+    public GameObject canvas;
+    public VehicleCamera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        view =GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+        if(view.isMine){
+            localCam.SetActive(true);
+            
+            this.GetComponent<VehicleControl>().enabled = true;
+            this.GetComponent<VehicleDamage>().enabled = true;
+            canvas.SetActive(true);
+            // control1.SetActive(true);
+        }
+        else{
+            localCam.SetActive(false);
+            this.GetComponent<VehicleControl>().enabled = false;
+            this.GetComponent<VehicleDamage>().enabled = false;
+            canvas.SetActive(false);
+            
+            // control1.SetActive(false);
+        }
+        
+    }
+}
